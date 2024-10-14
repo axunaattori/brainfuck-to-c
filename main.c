3650c5692a2b63f30d77e48cfd5a1d881a7aadcc
axunaattori <69193142+axunaattori@users.noreply.github.com>
2024-10-15 02:29:01 +0300

    Update main.c
    actually add some of the features i planned for the future

--- main.c
+++ main.c
@@ -4,45 +4,129 @@
 
 #define ARRAY_SIZE 32000
 
-void switchcase(char character, FILE *outputFile)
+void addchar(FILE *outputFile, int type, int amount)
+{
+  switch(type)
+  {
+    case 1:
+      if(amount != 1)
+      {
+        fprintf(outputFile, "ptr+=%d;\n", amount);
+      }
+      else
+      {
+        fprintf(outputFile, "ptr++;\n");
+      }
+      break;
+    case 2:
+      if(amount != 1)
+      {
+        fprintf(outputFile, "ptr-=%d;\n", amount);
+      }
+      else
+      {
+        fprintf(outputFile, "ptr--;\n");
+      }
+      break;
+    case 3:
+      if(amount != 1)
+      {
+        fprintf(outputFile, "(*ptr)+=%d;\n", amount);
+      }
+      else
+      {
+        fprintf(outputFile, "(*ptr)++;\n");
+      }
+      break;
+    case 4:
+      if(amount != 1)
+      {
+        fprintf(outputFile, "(*ptr)-=%d;\n", amount);
+      }
+      else
+      {
+        fprintf(outputFile, "(*ptr)--;\n");
+      }
+      break;
+    case 5:
+      fprintf(outputFile, "while (*ptr){\n"); // doesn't need, would fix errors too anyway
+      break;
+    case 6:
+      fprintf(outputFile, "}\n"); // doesn't need, would fix errors too anyway
+      break;
+    case 7:
+      for (int i = 0; i < amount; i++)
+      {
+        fprintf(outputFile, "putchar(*ptr);\n"); // doesn't need
+      }
+      break;
+    case 8:
+      for (int i = 0; i < amount; i++)
+      {
+        fprintf(outputFile, "scanf(\"%%c\", (char *)ptr); \n"); // doesn't need
+      }
+      break;
+  }
+  // just ignore it if it gets here
+}
+
+
+int switchcase(char character)
 {
   switch(character)
   {
     case '>':
-      fprintf(outputFile, "*ptr++;\n");
+      return 1;
       break;
     case '<':
-      fprintf(outputFile, "*ptr--;\n");
+      return 2;
       break;
     case '+':
-      fprintf(outputFile, "(*ptr)++;\n");
+      return 3;
       break;
     case '-':
-      fprintf(outputFile, "(*ptr)--;\n");
+      return 4;
       break;
     case '[':
-      fprintf(outputFile, "while (*ptr){\n");
+      return 5;
       break;
     case ']':
-      fprintf(outputFile, "}\n");
+      return 6;
       break;
     case '.':
-      fprintf(outputFile, "putchar(*ptr);\n");
+      return 7;
       break;
     case ',':
-      fprintf(outputFile, "scanf(\"%%c\", (char *)ptr); \n");
+      return 8;
       break;
   }
+  return 0;
 }
 
 //compile brainfuck to c!?!?!?!?!??!
 void compilebf(const char *source, FILE *outputFile)
 {
-    size_t length = strlen(source);
-    
-    for (int i = 0; i < length; i++) {
-       switchcase(source[i], outputFile); 
+  //quit being a lazy fuck and actually code the features that i wanted 3 months ago
+  int type = 0;
+  int prevtype = 0;
+  int amount = 0;
+
+  size_t length = strlen(source);
+  for (int i = 0; i < length; i++)
+  {
+    type = switchcase(source[i]);
+    if(type == prevtype)
+    {
+      amount++;
     }
+    else
+    {
+      addchar(outputFile, prevtype, amount);
+      amount = 1;
+    }
+    prevtype = type;
+  }
+  addchar(outputFile, prevtype, amount);
 }
 
 int main(int argc, char *argv[])
